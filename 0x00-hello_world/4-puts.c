d5a7758261425bbbf571734111313161df3e0455
Bimbonium <bimbonium2@gmail.com>
2022-09-08 23:15:36 +0000

    First C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ * Main function - to house the code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
+}
