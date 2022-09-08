c188771aca6162642f10bdaf3667799622a942e3
Bimbonium <bimbonium2@gmail.com>
2022-09-08 23:22:47 +0000

    test printf and puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include <stdio.h>
 /**
  * Main function - to house the code
  * Return: 0
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *Main function - function to test printf
+ *return 0
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
