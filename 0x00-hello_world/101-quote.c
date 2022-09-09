19be4d30f3dd154948adcd38e838679fd5ce426c
Bimbonium <bimbonium2@gmail.com>
2022-09-09 00:22:47 +0000

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - program to test replacing standard error
+ *Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	Return(1);
+}
