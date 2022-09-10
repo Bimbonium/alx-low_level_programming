b1879f6c4ebe35bdc4e9bac45179ed5c8db83759
Bimbonium <bimbonium2@gmail.com>
2022-09-10 00:45:44 +0000

    Solve task 0 and added README

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - function for printing last digit of a number n
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+This repo is to warehose my foray into the branch and loop
+space. If and while to be specific
+~Bimbonium out
