138474c5c0e26c8b44b097aa918f81765ceeaa86
Bimbonium <bimbonium2@gmail.com>
2022-09-10 01:37:30 +0000

    Solve some character tasks using for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print lowercase characters
+ * Return:0
+ */
+
+int main(void)
+{
+
+	/*using a for loop*/
+	char a;
+
+	for (a = a; a <= z; a++)
+		putchar(a);
+	putchar("\n");
+	return (0);
+}
+
+/** No reason not to declare a variable a i.e. char a
+	 *  that will warehouse all the alphabets and I will just putchar
+	 *  for a; somethins like
+	 *  char a = 'abcdefghijklmnopqrstuvwxyz'
+	 *  putchar(a)
+	 *  putchar("\n")
+	 *  This is not a dynamic code but it works just fine in my opinion
+*/
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function to print alphabets in upper and lower case
+ * Return:0
+ *Betty is really beginning to get on my nerves
+ */
+
+int main(void)
+{
+
+	char a;
+	char b;
+
+	for(a=a; a<=z; a++)
+		putchar(a);
+	for(b=A; A<=Z; A++)
+		putchar(b);
+	putchar("\n");
+	
+}
