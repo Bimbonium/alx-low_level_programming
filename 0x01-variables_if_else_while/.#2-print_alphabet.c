157ca983c1c74832c83c4c7afcc9e114d371e11b
Bimbonium <bimbonium2@gmail.com>
2022-09-10 01:40:52 +0000

    Alter task 2 and 3

---
+++ 0x01-variables_if_else_while/.#2-print_alphabet.c
@@ -0,0 +1,1 @@
+vagrant@ubuntu-focal.4286:1662769466
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,6 +4,8 @@
  * main - function to print alphabets in upper and lower case
  * Return:0
  *Betty is really beginning to get on my nerves
+ *Again this code would have been better using just a straight
+ *character declaration, even though it will not be dynamic
  */
 
 int main(void)
@@ -12,10 +14,10 @@ int main(void)
 	char a;
 	char b;
 
-	for(a=a; a<=z; a++)
+	for (a = a; a <= z; a++)
 		putchar(a);
-	for(b=A; A<=Z; A++)
+	for (b = A; A <= Z; A++)
 		putchar(b);
 	putchar("\n");
-	
+	return (0);
 }
