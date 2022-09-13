35e69768948d2391cb4dba015f551afdb6ab598d
Bimbonium <bimbonium2@gmail.com>
2022-09-13 23:21:28 +0000

    Do main.h, _putchar, and 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include main.h
+
+/**
+ * main - function to print putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	unsigned char a = '_putchar';
+
+	_putchar(a);
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* using MAIN_H in place of main.h defining it as a 'macro'*/
+int _putchar (char c);
+
+
+
+
+#endif
