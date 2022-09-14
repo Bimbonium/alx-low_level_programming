399efd0a4986098c1409d7274b70050076934812
Bimbonium <bimbonium2@gmail.com>
2022-09-14 22:58:49 +0000

    Make some corrections

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,18 @@
 int main(void)
 {
 	int i;
+
 	long double sum = 1.0;
 	long double j = 2.0;
 	long double k;
 
-	printf("%.0lf, ", sum);
+	printf("%.0Lf, ", sum);
 	for (i = 1; i < 98; i++)
 	{
 		k = sum + j; /* at first run, sum =3*/
 		sum = j;
 		j = k;
-		printf("%.0lf", sum);
+		printf("%.0Lf", sum); /* use capital L for long double*/
 
 		if (i != 97)
 		{
