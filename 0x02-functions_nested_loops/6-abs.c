08a2e98a06483c054b51fc9cc89ca262b87b4e21
Bimbonium <bimbonium2@gmail.com>
2022-09-14 00:50:31 +0000

    Solve a bunch of tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+include "main.h"
+/**
+ * _abs - function to get absolute
+ * @int: any integer passed into the function
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int c;
+
+	if (c >= 0)
+		return (c);
+	else if (c < 0)
+		return (c * -1);
+
+}
