a0aec0452b2ddb02684e43a13f4dedce180bffea
Bimbonium <bimbonium2@gmail.com>
2022-09-14 00:06:57 +0000

    Solve task 1 through 3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
 #include main.h
 
 /**
- * main - function to print putchar
+ * main - function to print _putchar
+ * using _putchar
  * Return: 0
  */
 
 int main(void)
 {
 
-	unsigned char a = '_putchar';
-
-	_putchar(a);
+	_putchar ('-');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
 	_putchar('\n');
 
 	return (0);
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include main.h
+/*
+ * print_alphabet - function to print the alphabets
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar (a);
+	_putchar ('\n')
+	return (0);
+}
