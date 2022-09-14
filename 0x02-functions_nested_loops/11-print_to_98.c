775cd2248efbbcc7e093259a7fdeaca3c422d711
Bimbonium <bimbonium2@gmail.com>
2022-09-14 02:15:43 +0000

    Solve a bunch of tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,14 @@ void print_to_98(int n)
 		printf("%d", n);
 	else
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			if (n < 0)
 				n *= -1;
 			printf("%d, ", n);
 		}
 
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
