3446e99513f264a5c23a16275ed8d8682761bf74
Bimbonium <bimbonium2@gmail.com>
2022-09-14 01:28:28 +0000

    Solve a bunch of tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to print last digit
+ * @c: argument passed into funciton
+ * Return: last digit
+ */
+
+int print_last_digit(int c)
+{
+
+	return (c % 10);
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - 9 times table function
+ *
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int product = j * i;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
