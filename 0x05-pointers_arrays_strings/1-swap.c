2ffa59474dd2702fcdfa19c596bc3836adaa104f
Bimbonium <bimbonium2@gmail.com>
2022-09-20 00:51:42 +0000

    Solve task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two integers
+ * @a: integer 1
+ * @b: integer 2
+ */
+
+void swap_int(int *a, int *b);
+{
+
+	int c; /* temp integer */
+
+	c = a;
+	*a = b;
+	*b = c;
+
+}
