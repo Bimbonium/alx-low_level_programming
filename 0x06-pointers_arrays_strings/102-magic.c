6e9a9816a6730b609feffdc9c3bcdc72f3a02f57
Bimbonium <bimbonium2@gmail.com>
2022-09-23 11:13:56 +0000

    Tests to task 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p - 3) = 98;
+  printf("the value of p and a are %x and %x respectfully\n", p, a);
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
