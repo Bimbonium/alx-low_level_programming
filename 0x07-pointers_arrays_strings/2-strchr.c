d7a0de71015a6089787f28e83d151520b68d2b68
Bimbonium <bimbonium2@gmail.com>
2022-09-27 00:36:49 +0000

    Solve task 3 and edited task 2 to include climments

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,14 +13,14 @@ char *_strchr(char *s, char c)
 
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++) /*check through the string*/
 	{
-		if (s[i] == c)
+		if (s[i] == c) /*at first occurence of c, break!*/
 		{
 			break;
 		}
 	}
 	if (s[i] == c)
-		return (s + i);
-	return (0);
+		return (s + i); /*return memory location for first occurence*/
+	return (0); /*else return 0/null */
 }
