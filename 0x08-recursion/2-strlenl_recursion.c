a3c3503ee93b171251d6b3aa8d5c5761245d5b51
Bimbonium <bimbonium2@gmail.com>
2022-09-28 23:14:38 +0000

    Update task 1 and Solve 2

---
+++ 0x08-recursion/2-strlenl_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - get length of string s
+ * @s: string to get it's length
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s);
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i = _strlen_recursion(s + 1);
+		return (i++);
+	}
+
+	return (0);
+}
