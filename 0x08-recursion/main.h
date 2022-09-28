4d2b95dafdc5b12a126ef5639a7a2d09f4ab6ae2
Bimbonium <bimbonium2@gmail.com>
2022-09-28 23:02:36 +0000

    Solve task 0 and 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,13 @@
 /* using MAIN_H in place of main.h defining it as a 'macro'*/
 
 int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 
 #endif
