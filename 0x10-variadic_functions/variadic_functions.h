8f7ed82da17b26ca958eb4f653596081ec6e3e82
Bimbonium <bimbonium2@gmail.com>
2022-10-13 17:53:52 +0000

    Retry task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,25 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARIADICFUNCTIONSH
+#define VARIADICFUNCTIONSH
 #include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int print_c(va_list a);
-int print_i(va_list a);
-int print_f(va_list a);
-int print_s(va_list a);
+
+int _putchar(char c);
+
 /**
- * struct op - struct for 3
- * @c: character to check
- * @f: function to point to
+ * struct funckey - a key for picking functions to use for printing
+ * in 3-print_all.c
+ *
+ * @f: function to use for printing
+ * @spec: character specifier
  */
-typedef struct op
+typedef struct funckey
 {
-	char *c;
-	int (*f)(va_list a);
-} printer;
+	void (*f)(va_list);
+	char spec;
+} funckey;
+
 #endif
