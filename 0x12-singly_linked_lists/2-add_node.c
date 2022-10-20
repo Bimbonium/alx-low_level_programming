dfe7552c9b5835319db6f2a9787824cfa7d4cb02
Bimbonium <bimbonium2@gmail.com>
2022-10-20 16:55:25 -0700

    Solve  task 2 using strdup

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node - function to add node at the beginning of a list
@@ -12,20 +13,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	int length = 0;
-	char *str2;
-	list_t *new_node;
+       	list_t *new_node;
 
 	for (length = 0; str[length] != '\0'; length++)
 	{
-		str2[length] = str[length];
 	}
-	str2[length] = '\0';
+
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = str2;
+	new_node->str = strdup(str);
 	new_node->len = length;
 	new_node->next = *head;
 	*head = new_node;
