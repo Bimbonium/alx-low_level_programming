f923df6db65c2b93907d03a9b3cb9cbbb9e17648
Bimbonium <bimbonium2@gmail.com>
2022-10-20 18:16:48 -0700

    Solve task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void __attribute__ ((constructor)) turtle()
 {
 
 	printf("You're beat! and yet, you must allow,\n"
-	"I bore my house upon my back!\n")
+	       "I bore my house upon my back!\n");
 }
