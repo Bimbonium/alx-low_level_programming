27056d6abb7e60eafcdb765f10dc88ce12fa8cac
Bimbonium <bimbonium2@gmail.com>
2022-10-24 18:21:22 -0700

    solve task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *free_listint - function to free lists
+ *free_listint2 - function to free lists
  *@head: pointer to the head node
  */
 
-void free_listint(listint_t **head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
